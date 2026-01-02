a72bbe5a48775f32dbb3e7adb14bee608dae7ef1
Ivan Pribec <ivan.pribec@gmail.com>
2026-01-03 00:49:08 +0100

    Add another banded example

---
+++ bandfs/bandfs_demo.f90
@@ -0,0 +1,40 @@
+program bandfs_demo
+  implicit none
+
+  integer, parameter :: wp = kind(1.0d0) ! Double precision
+
+  ! BANDFS procedure interfaces
+  include "bandfs.fi"
+
+  integer, parameter :: n = 5, m = 3  ! Dimension and bandwidth
+  integer, parameter :: h = (m-1)/2   ! Half-bandwidth
+  
+  ! Array storage
+  real(wp) :: a(n,-h:h+h), b(n)
+  integer  :: ipiv(n), info
+
+  ! 1. Initialize array and RHS
+  ! Gilbert Strang's favorite matrix (second order differences)
+  a(:,-1) = [ 0,-1,-1,-1,-1]  ! Lower diagonal
+  a(:, 0) = [ 2, 2, 2, 2, 2]  ! Main diagonal
+  a(:, 1) = [-1,-1,-1,-1, 0]  ! Upper diagonal
+
+  ! Right-hand side for a target solution of x = [1, 2, 3, 4, 5]
+  b = [ 0, 0, 0, 0, 6]
+
+  ! 2. Perform LU Factorization
+  ! 'a' is modified in-place; 'ipiv' stores pivoting sequence
+  call bandf(a, m, n, ipiv, info)
+
+  if (info /= 0) then
+     write(*,'(A)') "Error: Matrix is singular or factorization failed."
+     stop 1
+  end if
+
+  ! 3. Solve the system
+  ! 'b' is overwritten with the solution vector 'x'
+  call bands(a, b, m, n, ipiv)
+
+  write(*,'(A,/,*(2X,F6.3,:,/))') "Solution x:", b
+
+end program
